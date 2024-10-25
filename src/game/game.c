a65a6663484a6b9560aca9dbb2139aff429cb79b
cursedastronaut <g.martineaux@student.isartdigital.com>
2024-10-25 15:05:09 +0200

    Removed pointer types on pointer types

--- src/game/game.c
+++ src/game/game.c
@@ -2,10 +2,10 @@
 #include "animatronics.h"
 void Update(Player *player, Freddy *freddy, Foxy *foxy, BonChi *bonnie, BonChi *chica) {
 	if (player->deltaTime < RUNNING_TPS) return;
-	freddy_processing(&player, &freddy);
-	bonnie_processing(&player, &bonnie);
-	foxy_processing(&player, &foxy);
-	player_processing(&player);
+	freddy_processing(player, freddy);
+	bonnie_processing(player, bonnie);
+	foxy_processing(player, foxy);
+	player_processing(player);
 }
 
 int randB(unsigned int min, unsigned int max) {
